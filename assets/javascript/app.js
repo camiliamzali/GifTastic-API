9e77177dffe783b28720dafde0e67ec9e77b3a6a
Camilia <mzalicam@gmail.com>
2019-04-09 18:15:21 -0400

    gifs now populate gif div when a button is added and clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,54 +2,94 @@ $(document).ready(function () {
 
   var term = [];
 
-  function createURL() {
-    queryURL = `https://api.giphy.com/v1/gifs/search?`
 
-    queryParams = {
-      "api_key": "J40tZPfBSGRJp1iF5AwOdK5grc2qy5jk"
-    }
+  function displayAnimalGifs() {
+    var storeTerm = $(this).attr("data-term");
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${storeTerm}&api_key=J40tZPfBSGRJp1iF5AwOdK5grc2qy5jk&limit=10`
 
-    queryParams.q = $("#term").val().trim();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response)
+      // create div to hold the gifs
 
-    console.log(`
-    ---------------
-    URL: ${queryURL}
-    ---------------`);
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+
+        var gifDiv = $(`<div class="gif">`)
+
+        var gifURL = results[i].images.fixed_height.url;
+
+        var gifImage = $(`<img class="col-md-6">`);
+
+        gifImage.attr({
+          "src": gifURL,
+          "data-still": results[i].images.fixed_height_still.url,
+          "data-animate": gifURL,
+          "data-state": "animate"
+        });
+        var rating = $("<p>").text(`Rating: ${results[i].rating}`);
+
+        gifDiv.append(gifImage);
+        gifDiv.append(rating);
+
+        $("#gifs").prepend(gifDiv);
+      }
+    });
+
   }
 
   function renderButtons() {
     $("#buttons").empty();
 
     for (var i = 0; i < term.length; i++) {
-      $("#buttons").append(`<button>${term[i]}</button>`)
+
+      var gifButton = $("<button>");
+      gifButton.addClass("gif-button btn btn-lg btn-outline-dark text-capitalize");
+      gifButton.attr("data-term", term[i]);
+      gifButton.text(term[i]);
+
+      $("#buttons").append(gifButton);
+
+
     }
 
 
   }
 
 
-  $("#submit").on("click", function () {
+  $("#form").on("submit", function () {
     event.preventDefault();
-    var userInput = $("#term").val();
+
+    var userInput = $("#term").val().trim();
+
     if (userInput === "") {
       return false;
     }
+
     term.push(userInput);
 
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // }).then(function (response) {
-    //   console.log(response)
+
     renderButtons();
     $("#term").val("");
-    // });
+  });
+
+
+  $(document).on("click", ".gif-button", displayAnimalGifs);
+
+  $("img").on("click", function () {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
   });
   renderButtons();
-
-  createURL();
-
 
 });
